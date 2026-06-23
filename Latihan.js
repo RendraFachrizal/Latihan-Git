e529e8e41ebf55acfac76a78d84471ea7a4fc842
RendraFachrizal <rdrfachrizal@gmail.com>
2026-06-23 09:22:05 +0800

    Aritmatika

--- Latihan.js
+++ Latihan.js
@@ -5,7 +5,30 @@ var NomorUrut = 12;
 
 console.log(nama);
 console.log(NomorUrut);
-console.log("nama");
 console.log("Nomor Urut: " + NomorUrut + ", Nama: " + nama);
 
+var Open = false;
+if (Open == true) {
+    console.log("Buka");
+} else {
+    console.log("Tutup");
+}
 
+
+// Operator Aritmatika
+var a = 100
+var b = 50
+
+var tambah = a + b;
+var kurang = a - b;
+var kali = a * b;
+var bagi = a / b;
+var modulus = a % b; 
+
+console.log("a: " + a);
+console.log("b: " + b);
+console.log("Hasil Penjumlahan: " + tambah);
+console.log("Hasil Pengurangan: " + kurang);
+console.log("Hasil Perkalian: " + kali);
+console.log("Hasil Pembagian: " + bagi);
+console.log("Hasil Modulus: " + modulus);
