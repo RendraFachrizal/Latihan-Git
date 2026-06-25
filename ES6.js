e04b27e352afca7347c4b2bb5737f914245dd78e
RendraFachrizal <rdrfachrizal@gmail.com>
2026-06-25 14:46:49 +0800

    Let + Constraint

---
+++ ES6.js
@@ -0,0 +1,36 @@
+//let and const
+
+// let nama = "John"
+// if(true) 
+//     {
+//         let nama = "Doe"
+//         console.log(nama)
+//     }
+// console.log(nama)
+// console.log("-------------------------------")
+
+// const pi = 3.14
+// pi = 22/7
+// console.log(pi)
+
+// //arrow function
+// var tampilkanNama = function(a) {
+//     console.log("Halo, " + (a))
+
+// }
+// tampilkanNama("Huda")
+
+// const sayHello = (a) => {
+//     console.log("Halo, "+ a)
+// }
+// sayHello("Rendra")
+
+//Quiz
+const HitungPersergi = (sisi) => {
+    const luas = (sisi) => {s * s}
+    const keliling = (sisi) => {4 * s}
+    console.log("Luas Persegi: ", (sisi));
+    console.log("Keliling Persegi: ", (sisi));
+}
+
+HitungPersergi(5)
