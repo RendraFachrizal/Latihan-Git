f20d4edf464aea099d6a2382e5d931dd77e7ef83
RendraFachrizal <rdrfachrizal@gmail.com>
2026-06-25 09:57:41 +0800

    Function

--- Function.js
+++ Function.js
@@ -1,61 +1,61 @@
-// //function tanpa return
+//function tanpa return
 
-// function tampilkan(){
-//     console.log("Halo! ");
-// }
+function tampilkan(){
+    console.log("Halo! ");
+}
 
-// tampilkan();
+tampilkan();
 
-// console.log("--------------------------------")
-// //function dengan return
-// function MunculkanAngkaDua() {
-// return 2
-// }
+console.log("--------------------------------")
+//function dengan return
+function MunculkanAngkaDua() {
+return 2
+}
 
-// var tampung = MunculkanAngkaDua();
-// console.log(tampung)
-
-
-// console.log("--------------------------------")
-// //function dengan parameter
-// console.log("Contoh 3: ")
-// function kalikanDua(angka) {
-//     return angka*2
-// }
-
-// var tampung = kalikanDua(2);
-// console.log(tampung)
+var tampung = MunculkanAngkaDua();
+console.log(tampung)
 
 
-// console.log("--------------------------------")
-// //Function dengan lebih dari satu dan nilai default
-// console.log("Contoh 4")
-// function tampilkanAngka(angkapertama, angkakedua = 2) {
-// return angkapertama + angkakedua
-// }
+console.log("--------------------------------")
+//function dengan parameter
+console.log("Contoh 3: ")
+function kalikanDua(angka) {
+    return angka*2
+}
 
-// console.log(tampilkanAngka(5,3))
-// console.log(tampilkanAngka(6))
-
-
-// console.log("--------------------------------")
-// //test case
-// function tampilkanAngka(angkapertama, angkakedua = 2, angkaketiga = 3) {
-// return angkapertama + angkakedua + angkaketiga
-// }
-
-// console.log(tampilkanAngka(5,3))
-// console.log(tampilkanAngka(6))
-// console.log(tampilkanAngka(123,2))
+var tampung = kalikanDua(2);
+console.log(tampung)
 
 
-// console.log("--------------------------------")
-// //Quiz
-// function tampilkanNama(Nama = "Rendra") {
-//     console.log("Halo! " + Nama)
-// }
+console.log("--------------------------------")
+//Function dengan lebih dari satu dan nilai default
+console.log("Contoh 4")
+function tampilkanAngka(angkapertama, angkakedua = 2) {
+return angkapertama + angkakedua
+}
 
-// tampilkanNama()
+console.log(tampilkanAngka(5,3))
+console.log(tampilkanAngka(6))
+
+
+console.log("--------------------------------")
+//test case
+function tampilkanAngka(angkapertama, angkakedua = 2, angkaketiga = 3) {
+return angkapertama + angkakedua + angkaketiga
+}
+
+console.log(tampilkanAngka(5,3))
+console.log(tampilkanAngka(6))
+console.log(tampilkanAngka(123,2))
+
+
+console.log("--------------------------------")
+//Quiz
+function tampilkanNama(Nama = "Rendra") {
+    console.log("Halo! " + Nama)
+}
+
+tampilkanNama()
 
 //Anonymous Function
 console.log("-----------------------------------------")
