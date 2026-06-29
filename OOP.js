cefcec4ebb12bab3bf8c36fab9c5dd3b11a64bed
RendraFachrizal <rdrfachrizal@gmail.com>
2026-06-29 09:46:02 +0800

    Quiz

--- OOP.js
+++ OOP.js
@@ -1,10 +1,51 @@
-var Car = class {
-    constructor (brand, factory) {
-        this.brand = brand
-        this.factory = factory
-        this.sound = "honk! honk! vroomvroom"
+//Contructor + instance class
+
+// var Car = class {
+//     constructor (brand, factory) {
+//         this.brand = brand
+//         this.factory = factory
+//         this.sound = "honk! honk! vroomvroom"
+//     }
+// }
+
+// var innova = new Car("Innova", "Toyota")
+// console.log(innova)
+
+//Method
+// class Car1 {
+//     constructor(brand) {
+//         this.carname = brand;
+//     }
+//     present () {
+//         return "I have a " + this.carname;
+//     }
+// }
+
+// mycar = new Car1("Ford");
+// console.log(mycar.present())
+
+// class Car {
+//     constructor(brand) {
+//         this.carname = brand;
+//     }
+//     present(x) {
+//         return x + "! I have a " + this.carname;
+//     }
+// }
+
+// mycar = new Car("Ford")
+// console.log(mycar.present("Hello"));
+
+
+//Quiz
+class Car {
+    constructor(brand) {
+        this.carname = brand;
+    }
+    present(x) {
+        return x + "! I have a " + this.carname;
     }
 }
 
-var innova = new Car("Innova", "Toyota")
-console.log(innova)
+mycar = new Car("Ford" + " and" +  " Toyota")
+console.log(mycar.present("Hello"));
