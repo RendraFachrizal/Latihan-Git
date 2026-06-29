97e2d95236f7258078dbf5e54f1eff75aa56367e
RendraFachrizal <rdrfachrizal@gmail.com>
2026-06-29 15:08:18 +0800

    database sql

---
+++ db.sql
@@ -0,0 +1,24 @@
+create TABLE mahasiswa (
+    nim int(10) primary key,
+    nama char (255) not null,
+    prodi char (255) not null,
+    jenisKelamin ENUM ('Laki - laki', 'Perempuan'),
+    alamat text not null,
+    noHP char(25) not null
+);
+
+INSERT INTO mahasiswa(nim, nama, prodi, jenisKelamin, alamat,noHP)
+VALUES(123456, "Muhammad Rendra Fachrizal", "TIK", "Laki - laki", "Samarinda", "087716515904");
+
+UPDATE mahasiswa
+SET alamat = "Balikpapan"
+WHERE NIM = 123456;
+
+DELETE FROM mahasiswa
+WHERE NIM = 123456
+
+INSERT INTO mahasiswa(nim, nama, prodi, jenisKelamin, alamat,noHP)
+VALUES(123654, "Rendra Fachrizal", "TIK", "Laki - laki", "Samarinda", "087716515904");
+
+INSERT INTO mahasiswa(nim, nama, prodi, jenisKelamin, alamat,noHP)
+VALUES(123465, "Fachrizal", "TIK", "Laki - laki", "Samarinda", "087716515904");
